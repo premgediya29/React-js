536637ce7bbfb637cab0e26da68ce25c9d51ca54
premgediya29 <160754206+premgediya29@users.noreply.github.com>
2024-05-21 17:49:10 +0530

    react p5

---
+++ p5 src/Template.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import  Nav  from './Componant/Nav'
+import  Header  from './Componant/Header'
+import Section1 from './Componant/Section1'
+import Section2 from './Componant/Section2'
+import Section3 from './Componant/Section3'
+import Footer from './Componant/Footer'
+
+function Template() {
+  return (
+    <>
+        <Nav/>
+        <Header/>
+        <Section1/>
+        <Section2/>
+        <Section3/>
+        <Footer/>
+    </>
+  )
+}
+
+export default Template
