3d9a5d4f0fe070b4f959b9a41f4280384e04d47e
premgediya29 <160754206+premgediya29@users.noreply.github.com>
2024-05-24 18:30:18 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,49 +1,24 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { increment , decrement } from "./redux/Actions";
+import { useSelector , useDispatch } from "react-redux";
 import './App.css';
-import Home from './Home';
-import Product from './Product';
-import { useState } from 'react';
-import Edit from './Edit';
 
-function App() {
-  const [posts, setPosts] = useState([])
+function App(){
 
-  function addnewPost(title,content,image){
-    let newPost = {
-      id : new Date().getTime(),
-      title : title,
-      content : content,
-      image : image
-    }
-    setPosts([...posts, newPost])
-   
-  }
+    const count = useSelector(state => state.count)
+    const dispatch = useDispatch()
 
-  function deletPost(id){
-    setPosts(posts.filter((el)=>(
-    el.id !== id
-    )))
-        }
-        
-        function updatePost(id,title,content,image) {
-          setPosts(posts.map(post => post.id === id ? {...post,title:title,content:content,image:image} : post));
-        }
-
-       
-      
-  return (
-    <Router>
-      <div className="App">
-     <Routes>
-      <Route path = "/" element = {<Home posts={posts} deletPost={deletPost} />} ></Route>
-
-      <Route path = "/add" element = {<Product addnewPost = {addnewPost}/>}></Route>
-        
-      <Route path='/edit/:id' element={<Edit updatePost={updatePost} posts={posts}/>} ></Route>
-     </Routes>
-     </div>
-    </Router>
-  );
+    return (
+      <>
+      <div class="box">
+        <center>
+          <h1> Counter App </h1>
+          <h1> Count : {count} </h1>
+          <button onClick={()=>{dispatch(increment())}}> + </button>
+          <button onClick={()=>{dispatch(decrement())}}> - </button>
+        </center> 
+      </div>
+      </>
+    );
 }
 
 export default App;
